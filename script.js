d43c0ae6d276fa49edd20037d18fabc9f7e1d67a
DundukRB <dikidangelange212@gmail.com>
2026-07-25 15:50:28 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const scriptText = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/DundukRB/Bestscript/refs/heads/main/script"))()';
+
+function copyScript() {
+    navigator.clipboard.writeText(scriptText);
+}
